8cbee673adbb4e9d7d76983e188adfa1e4241f11
Juandam1224 <128067027+Juandam1224@users.noreply.github.com>
2024-05-29 13:42:14 -0500

    hola

--- colombia_news/src/app/forget-passsword/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function ForgetPage(){
-    return (
-      <h1>ForgetPage </h1>
-    )
-  }
-  
--- colombia_news/src/app/forget-password/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function ForgetPage(){
-    return (
-      <h1>ForgetPage</h1>
-    )
-  }
